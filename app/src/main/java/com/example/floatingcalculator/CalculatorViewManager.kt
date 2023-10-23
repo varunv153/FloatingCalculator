ccc8f0e008238971c5495bae7582475d2d842aed
Varun Venkatesh <varunv153@gmail.com>
2023-10-23 16:51:26 +0530

    Basic calculation with bugs

--- app/src/main/java/com/example/floatingcalculator/CalculatorViewManager.kt
+++ app/src/main/java/com/example/floatingcalculator/CalculatorViewManager.kt
@@ -3,16 +3,46 @@ package com.example.floatingcalculator
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class CalculatorViewManager {
 
-    fun handleClickInCalculator (floatingView: View, button: Button ) {
+    fun handleClickInCalculator(floatingView: View, button: Button) {
         val editText: EditText = floatingView.findViewById(R.id.editText)
-        val currentText:String = editText.text.toString()
+        val currentText: String = editText.text.toString()
+
         if (currentText == "0" || currentText == "Error") {
-            editText.setText(button.text.toString())
+            if (button.text == "=") {
+            } else {
+                editText.setText(button.text.toString())
+            }
         } else {
-            editText.append(button.text.toString())
+            val lastChar = currentText.lastOrNull()
+
+            if (button.text == "=") {
+                try {
+                    val result = evaluateExpression(currentText)
+                    editText.setText(result.toString())
+                } catch (e: Exception) {
+                    editText.setText("Error")
+                }
+            } else if (isOperator(lastChar) && isOperator(button.text.toString()[0])) {
+                editText.text.delete(currentText.length - 1, currentText.length)
+            } else {
+                editText.append(button.text.toString())
+            }
+        }
+    }
+
+    private fun isOperator(char: Char?): Boolean {
+        return char == '+' || char == '-' || char == '*' || char == '/'
+    }
+
+    private fun evaluateExpression(expression: String): Double {
+        return try {
+            ExpressionBuilder(expression).build().evaluate()
+        } catch (e: Exception) {
+            Double.NaN
         }
     }
 }
