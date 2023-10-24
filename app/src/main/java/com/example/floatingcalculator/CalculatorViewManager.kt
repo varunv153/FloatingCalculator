21e065f783200439ef41ec81f8fc51a9efc3b59c
Varun Venkatesh <varunv153@gmail.com>
2023-10-24 16:20:08 +0530

    Refactored CalculatorViewManager

--- app/src/main/java/com/example/floatingcalculator/CalculatorViewManager.kt
+++ app/src/main/java/com/example/floatingcalculator/CalculatorViewManager.kt
@@ -11,46 +11,54 @@ class CalculatorViewManager {
     fun handleClickInCalculator(floatingView: View, button: Button) {
         val editText: EditText = floatingView.findViewById(R.id.editText)
         val currentText: String = editText.text.toString()
+        val lastChar = currentText.lastOrNull()
 
-        if (currentText == "0" || currentText == Double.NaN.toString()) {
-            if (button.text == "=") {
-            } else if (button.text == "del") {
-            } else {
-                editText.setText(button.text.toString())
-            }
-        } else {
-            val lastChar = currentText.lastOrNull()
-
-            if (button.text == "=") {
-                try {
-                    val result = evaluateExpression(currentText)
-                    editText.setText(result.toString())
-                } catch (e: Exception) {
-                    Log.e("Varun Floating Calculator", "Error while evaluating expression", e )
-                }
-            } else if (button.text == "del") {
-                if (currentText.isNotEmpty()) {
-                    editText.text.delete(currentText.length - 1, currentText.length)
-                }
-            } else {
-                if (isOperator(lastChar) && isOperator(button.text.toString()[0])) {
-                    editText.text.delete(currentText.length - 1, currentText.length)
-                }
-                editText.append(button.text.toString())
-            }
+        when (button.text) {
+            "=" -> handleEqualsButton(editText, currentText)
+            "del" -> handleDeleteButton(editText, currentText)
+            else -> handleOtherButtons(editText, currentText, lastChar, button)
         }
 
         val calculatorButton = floatingView.findViewById<Button>(R.id.delete_button)
-        calculatorButton.setOnLongClickListener { v ->
+        calculatorButton.setOnLongClickListener {
             editText.setText("")
             true
         }
+    }
 
+    private fun handleEqualsButton(editText: EditText, currentText: String) {
+        try {
+            val result: Double = evaluateExpression(currentText)
+            editText.setText(result.toString())
+
+        } catch (e: Exception) {
+            Log.e("Varun Floating Calculator", "Error while evaluating expression", e)
+        }
+    }
+
+    private fun handleDeleteButton(editText: EditText, currentText: String) {
+        if (currentText.isNotEmpty()) {
+            editText.text.delete(currentText.length - 1, currentText.length)
+        }
+    }
+
+    private fun handleOtherButtons(editText: EditText, currentText: String, lastChar: Char?, button: Button) {
+        if (currentText == "0" || currentText == "NaN") {
+            if (button.text != "=" && button.text != "del") {
+                editText.setText(button.text.toString())
+            }
+        } else {
+            if ( isOperator(lastChar) && isOperator(button.text[0])) {
+                editText.text.delete(currentText.length - 1, currentText.length)
+            }
+            editText.append(button.text.toString())
+        }
     }
 
     private fun isOperator(char: Char?): Boolean {
         return char != null && char in "+-*/"
     }
+
 
     private fun evaluateExpression(expression: String): Double {
         return try {
